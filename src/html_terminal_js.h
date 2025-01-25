aa3b88b25e6bdb5528c59196c258c847a04a1e43
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2025-01-25 21:13:05 +0100

    #10 Added a send event on the inputbox
    #10 Added a send event on the inputbox

--- src/html_terminal_js.h
+++ src/html_terminal_js.h
@@ -4,28 +4,37 @@ const uint8_t terminal_js[] = R"rawliteral(
    Source: https://github.com/spacehuhntech/WiFiDuck
  */
 // ===== WebSocket Actions ===== //
-function ws_connected() {}
+function ws_connected() { }
 
 // ===== Startup ===== //
-window.addEventListener("load", function() {
-  E("send").onclick = function() {
+function send_input_command()
+{
     var input = E("input").value;
-
     E("output").innerHTML += "# " + input + "<br>";
 
     E("reconnect").onclick = ws_init;
 
-    ws_send(input, function(msg) {
-      log(msg);
-      E("output").innerHTML += msg.replace(/\n/g, "<br>");
-      E("output").scrollTop = E("output").scrollHeight;
+    ws_send(input, function (msg) {
+        log(msg);
+        E("output").innerHTML += msg.replace(/\n/g, "<br>");
+        E("output").scrollTop = E("output").scrollHeight;
     });
-  };
+}
+window.addEventListener("load", function () {
+    E("send").onclick = send_input_command;
+    E("input").addEventListener("keyup", ({key}) =>  {
+        console.log("Key = " + key);
+        if (key === 'Enter' ) {
+            send_input_command();
+        }
+    });  
 
-  E("clear").onclick = function() {
-    E("output").innerHTML = "";
-  };
+    E("clear").onclick = function () {
+        E("output").innerHTML = "";
+    };
 
-  ws_init();
+    ws_init();
 }, false);
+
+
 )rawliteral";
