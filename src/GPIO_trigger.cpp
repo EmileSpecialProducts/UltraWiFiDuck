25c0188a280007752104d122041ac65bd1156507
EmileSpecialProducts <130773873+EmileSpecialProducts@users.noreply.github.com>
2025-09-05 11:20:01 +0200

    improved the debugging

--- src/GPIO_trigger.cpp
+++ src/GPIO_trigger.cpp
@@ -19,6 +19,7 @@ void GpioTrigger::begin(int ledpNum)
          pinMode(gpio_pins[f],INPUT_PULLUP);
         }
     }
+    delay(50); // 100K * 100nF = 10ms * 5= 50ms settle time
     for(f=0;f< gpio_pins_len ;f++)
     {
         pinstate[f]=digitalRead(gpio_pins[f]);
@@ -34,14 +35,16 @@ void GpioTrigger::update()
         {
             if(pinstate[f]!=digitalRead(gpio_pins[f]))
             {
+                debugf("GpioPin%d %d->",gpio_pins[f],pinstate[f]);
                 pinstate[f]=digitalRead(gpio_pins[f]);
-                debugf("Pin %d %d",gpio_pins[f],pinstate[f]);
+                debugf("%d\n",pinstate[f]);
                 char filename[10];
                 if(pinstate[f]==0)   
                     sprintf(filename,"/gpiolow%d",gpio_pins[f]);
                     else
                     sprintf(filename,"/gpiohi%d",gpio_pins[f]);
-                duckscripts_run(filename);
+                if(LittleFS.exists(filename))    
+                    duckscripts_run(filename);
             }
         }  
     } 
